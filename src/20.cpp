7911e6ff8f40b641eba654d0224ad208c390f844
peinehallie617 <peinehallie617@gmail.com>
2025-03-31 11:55:42 +0000

    Add 20.cpp

---
+++ src/20.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    double b = 3.14;
+    cout << "The value of a is: " << a << endl;
+    cout << "The value of b is: " << b << endl;
+    return 0;
+}
