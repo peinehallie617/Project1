eb9fc8e53a1e673075829f2acc25b3aa1b65e3d8
peinehallie617 <peinehallie617@gmail.com>
2025-04-03 20:37:35 +0000

    Add 23.cpp

---
+++ src/23.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    if (a > b) {
+        cout << "The value of a is greater than b." << endl;
+    } else {
+        cout << "The value of a is less than or equal to b." << endl;
+    }
+    return 0;
+}
